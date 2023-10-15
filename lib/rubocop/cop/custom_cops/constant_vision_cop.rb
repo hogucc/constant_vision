b442c65dc5ea33e7e7f77030e1e0623acd2e641f
hogucc <uku.h1r8@gmail.com>
2023-10-16 08:31:12 +0900

    main implementation of constant_vision_cop

--- lib/rubocop/cop/custom_cops/constant_vision_cop.rb
+++ lib/rubocop/cop/custom_cops/constant_vision_cop.rb
@@ -8,10 +8,34 @@ module RuboCop
       class ConstantVisionCop < Cop
         def on_const(node)
           constant_name = node.children[1].to_s
-          origin = ConstantVision.find_origin_of_constant(constant_name, Object)
+          namespace_nodes = find_namespace_nodes(node)
+          nodes = nodes_to_s(namespace_nodes)
+          namespace_path = if nodes.empty?
+            'Object'
+          else
+            nodes
+          end
+
+          origin = ConstantVision.find_origin_of_constant(constant_name, namespace_path)
+          return if origin.nil?
+
           candidates = ConstantVision.find_constant(constant_name)
           message = "Origin: #{origin}\nCandidates: #{candidates.join(', ')}"
           add_offense(node, message: message)
+        end
+
+        def nodes_to_s(nodes)
+          nodes.map { |node| node.children[0].children[1] }.reverse.join("::")
+        end
+
+        def find_namespace_nodes(node)
+          return [] unless node.parent
+
+          if (node.parent.type == :module || node.parent.type == :class) && node.parent.children[1] == node
+            [node.parent] + find_namespace_nodes(node.parent)
+          else
+            find_namespace_nodes(node.parent)
+          end
         end
       end
     end
