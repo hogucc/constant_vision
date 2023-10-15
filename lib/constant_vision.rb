d095bb3c012fa8b0e52fb53ef32d18c0236591eb
hogucc <uku.h1r8@gmail.com>
2023-10-15 14:33:22 +0900

    fixed some bugs

--- lib/constant_vision.rb
+++ lib/constant_vision.rb
@@ -4,27 +4,54 @@ require_relative 'constant_vision/version'
 
 module ConstantVision
   def self.find_origin_of_constant(constant_name, context)
-    namespaces = context.to_s.split('::')
-    base_constant = constant_name.to_s.split('::').first
+    Rails.application.eager_load! if defined?(Rails)
 
-    # 名前空間を逆順に走査して、該当する名前空間を検索
-    matching_namespace = namespaces.reverse_each.find do |namespace|
-      current_module = constant_from_string(namespaces[0..namespaces.index(namespace)].join('::'))
-      # 名前空間に該当する定数が存在するか、名前空間と定数名が一致する
-      current_module.const_defined?(base_constant, false) || namespace == base_constant
+    context_namespaces = context.to_s.split('::')
+    target_constant_names = constant_name.to_s.split('::')
+
+    # constant_nameが絶対パスで指定されている場合
+    if constant_name.start_with?('::')
+      absolute_module = constant_from_string(constant_name)
+      return absolute_module ? constant_name : "#{constant_name} not found in #{context}"
     end
 
-    # 該当する名前空間が見つかった場合
-    return constant_from_string(namespaces[0..namespaces.index(matching_namespace)].join('::')) if matching_namespace
+    # contextからトップレベルまでの全ての名前空間を探索
+    (context_namespaces.length + 1).downto(0) do |i|
+      current_context = context_namespaces.first(i).join('::')
+      current_module = constant_from_string(current_context)
+      next if current_module.nil?
 
-    # トップレベルでの検索
-    return Object if Object.const_defined?(base_constant, false)
+      # target_constant_namesを探索
+      target_module = target_constant_names.inject(current_module) do |mod, const_name|
+        break nil unless mod.const_defined?(const_name, false)
+        break mod.const_get(const_name, false)
+      end
 
-    nil
+      # target_moduleが見つかればその名前を返す
+      if target_module
+        return (current_context.empty? ? "" : "#{current_context}::") + target_constant_names.join('::')
+      end
+    end
+
+    # トップレベルから定数を検索
+    top_level_module = constant_from_string(constant_name)
+    return constant_name if top_level_module
+
+    # contextがクラスである場合、そのスーパークラスを探索
+    if context.is_a?(Class) && context.superclass
+      superclass_name = find_origin_of_constant(constant_name, context.superclass)
+      return "#{context}::#{constant_name}" unless superclass_name.include?("not found")
+    end
+
+    # 定数が見つからない場合、エラーメッセージを返す
+    "#{constant_name} not found in #{context}"
   end
 
   def self.constant_from_string(str)
-    str.split('::').inject(Object) { |mod, class_name| mod.const_get(class_name) }
+    str.split('::').reject(&:empty?).inject(Object) do |mod, class_name|
+      return nil unless mod.const_defined?(class_name, false)
+      mod.const_get(class_name, false)
+    end
   end
 
   def self.find_constant(constant_name)
@@ -35,13 +62,18 @@ module ConstantVision
     ObjectSpace.each_object(Module) do |mod|
       # Objectの直接の子定数を処理
       if mod == Object && Object.const_defined?(constant_name, false)
-        matching_modules << Object.const_get(constant_name)
+        matching_modules << Object.const_get(constant_name).to_s
         next
       end
 
       next if mod == Object # Objectのそれ以外の定数は処理から除外
 
-      matching_modules << mod.const_get(constant_name) if mod.const_defined?(constant_name, false) # falseを指定することで、継承チェーンを検索しない
+      # falseを指定することで、継承チェーンを検索しない
+      if mod.const_defined?(constant_name, false)
+        # モジュールの名前を取得し、定数名と連結
+        full_name = "#{mod.name}::#{constant_name}"
+        matching_modules << full_name
+      end
     end
 
     matching_modules
--- spec/constant_vision_spec.rb
+++ spec/constant_vision_spec.rb
@@ -16,12 +16,12 @@ RSpec.describe ConstantVision do
 
       it 'finds MY_CONST' do
         result = ConstantVision.find_constant('MY_CONST')
-        expect(result).to eq([DummyModule::MY_CONST])
+        expect(result).to eq(["DummyModule::MY_CONST"])
       end
 
       it 'finds ANOTHER_CONST' do
         result = ConstantVision.find_constant('ANOTHER_CONST')
-        expect(result).to eq([DummyModule::ANOTHER_CONST])
+        expect(result).to eq(["DummyModule::ANOTHER_CONST"])
       end
     end
 
@@ -58,12 +58,47 @@ RSpec.describe ConstantVision do
 
       it 'finds Bar' do
         result = ConstantVision.find_constant('Bar')
-        expect(result).to eq([Bar, Foo::Bar])
+        expect(result).to eq(["Bar", "Foo::Bar"])
       end
 
       it 'finds Bar::Baz' do
         result = ConstantVision.find_constant('Bar::Baz')
-        expect(result).to eq([Foo::Bar::Baz])
+        expect(result).to eq(["Foo::Bar::Baz"])
+      end
+    end
+
+    context 'when there are modules with overlapping namespaces' do
+      before(:all) do
+        module Zoo
+          module Mammals
+            module Cats
+              class Lion
+              end
+            end
+          end
+        end
+
+        module Staff
+          module Mammals
+            class Cats
+            end
+          end
+        end
+      end
+
+      after(:all) do
+        Object.send(:remove_const, :Zoo)
+        Object.send(:remove_const, :Staff)
+      end
+
+      it 'finds Mammals::Cats::Lion' do
+        result = ConstantVision.find_constant('Mammals::Cats::Lion')
+        expect(result).to eq(["Zoo::Mammals::Cats::Lion"])
+      end
+
+      it 'finds Mammals::Cats' do
+        result = ConstantVision.find_constant('Mammals::Cats')
+        expect(result).to eq(["Staff::Mammals::Cats", "Zoo::Mammals::Cats"])
       end
     end
   end
@@ -82,7 +117,7 @@ RSpec.describe ConstantVision do
 
       it 'finds MY_CONST' do
         result = ConstantVision.find_origin_of_constant('MY_CONST', DummyModule)
-        expect(result).to eq(DummyModule)
+        expect(result).to eq("DummyModule::MY_CONST")
       end
     end
 
@@ -106,7 +141,7 @@ RSpec.describe ConstantVision do
 
       it 'finds MY_CONST' do
         result = ConstantVision.find_origin_of_constant('Bar::Piyo', Foo::Bar)
-        expect(result).to eq(Foo::Bar)
+        expect(result).to eq("Foo::Bar::Piyo")
       end
     end
 
@@ -132,9 +167,84 @@ RSpec.describe ConstantVision do
         Object.send(:remove_const, :Foo)
       end
 
-      it 'finds Foo::Bar' do
+      it 'finds Bar::Piyo' do
         result = ConstantVision.find_origin_of_constant('Bar::Piyo', Foo::Bar::Baz)
-        expect(result).to eq(Foo::Bar)
+        expect(result).to eq("Foo::Bar::Piyo")
+      end
+    end
+
+    context '別の名前空間に存在する定数を指定した場合' do
+      before do
+        module Zoo
+          module Mammals
+            module Cats
+              class Lion
+              end
+            end
+          end
+        end
+
+        module Staff
+          module Mammals
+            class Cats
+            end
+          end
+        end
+      end
+
+      after do
+        Object.send(:remove_const, :Staff)
+        Object.send(:remove_const, :Zoo)
+      end
+
+      it 'finds Mammals::Cats' do
+        result = ConstantVision.find_origin_of_constant('Mammals::Cats', 'Zoo::Mammals::Cats::Lion')
+        expect(result).to eq ("Zoo::Mammals::Cats")
+      end
+
+      it 'finds Mammals::Cats::Lion' do
+        result = ConstantVision.find_origin_of_constant('Mammals::Cats::Lion', 'Staff::Mammals::Cats')
+        expect(result).to eq ("Staff::Mammals::Cats::Lion")
+      end
+
+      it 'finds Zoo::Mammals' do
+        result = ConstantVision.find_origin_of_constant('Zoo::Mammals', 'Staff::Mammals::Cats')
+        expect(result).to eq ("Zoo::Mammals")
+      end
+
+      it 'finds Mammals::Cats' do
+        result = ConstantVision.find_origin_of_constant('Mammals::Cats', 'Staff::Mammals::Cats')
+        expect(result).to eq ("Staff::Mammals::Cats")
+      end
+
+      it 'finds ::Zoo::Mammals' do
+        result = ConstantVision.find_origin_of_constant('::Zoo::Mammals', 'Staff::Mammals::Cats')
+        expect(result).to eq ("::Zoo::Mammals")
+      end
+
+      it 'finds Mammals::Cats' do
+        result = ConstantVision.find_origin_of_constant('Mammals::Cats', 'Staff::Mammals::Cats')
+        expect(result).to eq ("Staff::Mammals::Cats")
+      end
+    end
+
+    context 'in superclass' do
+      before do
+        class Cats
+          CRY = 'meow'
+        end
+
+        class Lion < Cats; end
+      end
+
+      after do
+        Object.send(:remove_const, :Cats)
+        Object.send(:remove_const, :Lion)
+      end
+
+      it 'finds CRY' do
+        result = ConstantVision.find_origin_of_constant('CRY', Lion)
+        expect(result).to eq ("Lion::CRY")
       end
     end
   end
