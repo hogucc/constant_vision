0b8f3a8b54cd81c58b1b99091d1b56ad7ca6782f
hogucc <uku.h1r8@gmail.com>
2023-10-21 15:35:26 +0900

    translate commit messages from Japanese to English.

--- lib/constant_vision.rb
+++ lib/constant_vision.rb
@@ -9,41 +9,38 @@ module ConstantVision
     context_namespaces = context.to_s.split('::')
     target_constant_names = constant_name.to_s.split('::')
 
-    # constant_nameが絶対パスで指定されている場合
+    # if the constant_name is specified as an absolute path
     if constant_name.start_with?('::')
       absolute_module = constant_from_string(constant_name)
       return absolute_module ? constant_name : "#{constant_name} not found in #{context}"
     end
 
-    # contextからトップレベルまでの全ての名前空間を探索
+    # search all namespaces from context up to the top level.
     (context_namespaces.length + 1).downto(0) do |i|
       current_context = context_namespaces.first(i).join('::')
       current_module = constant_from_string(current_context)
       next if current_module.nil?
 
-      # target_constant_namesを探索
+      # explore the target_constant_names
       target_module = target_constant_names.inject(current_module) do |mod, const_name|
         break nil unless mod.const_defined?(const_name, false)
         break mod.const_get(const_name, false)
       end
 
-      # target_moduleが見つかればその名前を返す
       if target_module
         return (current_context.empty? ? "" : "#{current_context}::") + target_constant_names.join('::')
       end
     end
 
-    # トップレベルから定数を検索
+    # search for the constant from the top level.
     top_level_module = constant_from_string(constant_name)
     return constant_name if top_level_module
 
-    # contextがクラスである場合、そのスーパークラスを探索
     if context.is_a?(Class) && context.superclass
       superclass_name = find_origin_of_constant(constant_name, context.superclass)
       return "#{context}::#{constant_name}" unless superclass_name.include?("not found")
     end
 
-    # 定数が見つからない場合、エラーメッセージを返す
     "#{constant_name} not found in #{context}"
   end
 
@@ -60,15 +57,13 @@ module ConstantVision
     matching_modules = []
 
     ObjectSpace.each_object(Module) do |mod|
-      # Objectの直接の子定数を処理
       if mod == Object && Object.const_defined?(constant_name, false)
         matching_modules << Object.const_get(constant_name).to_s
         next
       end
 
-      next if mod == Object # Objectのそれ以外の定数は処理から除外
+      next if mod == Object
 
-      # falseを指定することで、継承チェーンを検索しない
       if mod.const_defined?(constant_name, false)
         # モジュールの名前を取得し、定数名と連結
         full_name = "#{mod.name}::#{constant_name}"
